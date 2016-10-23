fada24231b4d4116f51c0965e3c5540fc6515d5f
LP Boucher <lp_boucher@hotmail.com>
2016-10-23 16:01:19 +0100

    Added live autoreload via gulp-connect dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,20 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'builds/development/',
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/development/*.html')
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch('builds/development/*.html', ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
