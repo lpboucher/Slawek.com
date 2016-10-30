f1491e626b1214d60efcc386f99abcbabc63dfe6
LP Boucher <lp_boucher@hotmail.com>
2016-10-30 21:10:58 +0000

    Added dependecies to Browserify and Concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat');
 
 gulp.task('connect', function() {
   connect.server({
@@ -14,9 +16,17 @@ gulp.task('html', function() {
     .pipe(connect.reload())
 });
 
+gulp.task('js', function() {
+  gulp.src('components/js/*.js')
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload())
+});
+
 gulp.task('watch', function() {
   gulp.watch(['builds/development/*.html',
                 'builds/development/css/*.css'], ['html']);
+  gulp.watch('components/js/*.js', ['js']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'html', 'js', 'watch']);
