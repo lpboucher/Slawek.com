c17c63d7f5ec4e0444621bd17985c90f4c0a2970
LP Boucher <lp_boucher@hotmail.com>
2016-11-06 18:04:15 +0000

    Fixed bug where slider scrolling arrows would appear when overlay was active

--- components/js/overlay.js
+++ components/js/overlay.js
@@ -2,12 +2,16 @@
 function openContact() {
     document.getElementById("overlay").style.height = "100%";
     document.body.className+='modal-open';
+    document.getElementById("scrollleft").style.display = "none";
+    document.getElementById("scrollright").style.display = "none";
 }
 
 /* Close */
 function closeContact() {
     document.getElementById("overlay").style.height = "0%";
     document.body.classList.remove('modal-open');
+    document.getElementById("scrollleft").style.display = "inline-block";
+    document.getElementById("scrollright").style.display = "inline-block";
 }
 /* Adjust text area in contact form automatically */
 function adjust_textarea(h) {
