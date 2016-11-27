da03fb566575c23a7b94354f311188fe95ed1d5b
LP Boucher <lp_boucher@hotmail.com>
2016-11-27 11:33:39 +0000

    Created grid layout for gallery, added header

--- builds/development/gallery.php
+++ builds/development/gallery.php
@@ -12,16 +12,35 @@
 </head>
 
 <body>
-    <div class="gallerycontainer">
-        <ul id="">
-            <li><img class="" src="images/gallery/Workspace.jpg"></li>
-            <li><img class="" src="images/gallery/Figues.png"></li>
-            <li><img class="" src="images/gallery/strawberrycocktail.jpg"></li>
-            <li><img class="" src="images/gallery/afternoontea.jpg"></li>
-            <li><img class="" src="images/gallery/kwestsuite.jpg"></li>
-            <li><img class="" src="images/gallery/lifestyle.jpg"></li>
-            <li><img class="" src="images/gallery/raspberrycocktail.jpg"></li>
+
+    <div class="headerwrapper">
+        <!-- Commenting out on page for now
+        <h1>VIEW FINDER PHOTOGRAPHY</h1>
+        <h2>Slawek Tomas</h2> -->
+        <div id="menuwrapper">
+            <ul class="topmenu" id="myTopnav">
+              <li><a href="gallery.php">Galleries</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="javascript:void(0)" onclick="openAbout()">About</a></li>
+              <li><a href="javascript:void(0)" onclick="openContact()">Contact</a></li>
         </ul>
+        </div>
+    </div>
+
+    <div class="gallerycontainer">
+        <img class="" src="images/gallery/Workspace.jpg">
+        <img class="" src="images/gallery/Figues.png">
+        <img class="" src="images/gallery/strawberrycocktail.jpg">
+        <img class="" src="images/gallery/afternoontea.jpg">
+        <img class="" src="images/gallery/kwestsuite.jpg">
+        <img class="" src="images/gallery/lifestyle.jpg">
+        <img class="" src="images/gallery/raspberrycocktail.jpg">
+        <img class="" src="images/gallery/strawberrycocktail.jpg">
+        <img class="" src="images/gallery/afternoontea.jpg">
+        <img class="" src="images/gallery/kwestsuite.jpg">
+        <img class="" src="images/gallery/lifestyle.jpg">
+        <img class="" src="images/gallery/Workspace.jpg">
+        <img class="" src="images/gallery/Figues.png">
     </div>
 </body>
 </html>
